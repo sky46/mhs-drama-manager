ee1c1ff65defda0990675f12fb785841b8079bb9
Skyler Ma <skyler@skylerma.com>
2025-01-21 12:07:24 -0500

    Add basic query to backend server

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,14 @@ const { Pool } = require('pg');
 const app = express();
 const port = 3001;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const pool = new Pool({
+    connectionString: 'postgres://postgres:password@db:5432/postgres'
+})
+
+app.get('/', async (req, res) => {
+    var queryResult = await pool.query('SELECT * FROM test;')
+    res.send(`Hello World! ${JSON.stringify(queryResult.rows)}`)
+
 })
 
 app.listen(port, () => {
