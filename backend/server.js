c7ea2e59d2d6d5770d3f39241d141620fbb80d46
SKYLER MA <skyler@skylerma.com>
2025-02-05 09:20:45 -0500

    Implement user creation route in backend and change url path

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const { google } = require('googleapis');
 const session = require('express-session'); // To maintain user authentication
 const path = require('path');
 require('dotenv').config()
+const argon2 = require('argon2');
 
 const app = express();
 const port = process.env.PORT;
@@ -75,16 +76,31 @@ app.get('/', async (req, res) => {
 });
 
 // Post to create new user
-app.post('/users', async (req, res) => {
-    const { name, email, password } = req.body;
+app.post('/users/create', async (req, res) => {
+    const { name, email, password, passwordCheck, role } = req.body;
 
-    if (!name || !email || !password) {
+    if (!name || !email || !password || !passwordCheck || !role) {
         return res.status(400).json({ error: "Missing fields" }); // Missing fields
     }
-
+    if (password !== passwordCheck) {
+        return res.status(400).json({ error: "Passwords do not match" });
+    }
+    var roleID;
+    if (role === 'teacher') {
+        roleID = 0;
+    } else if (role === 'student') {
+        roleID = 1;
+    } else {
+        return res.status(400).json({ error: 'Invalid role' });
+    }
+    
+    const passwordHash = await argon2.hash(password);
+    const queryText = 'INSERT INTO users (name, email, password, role) VALUES ($1, $2, $3, $4) RETURNING *';
+    const queryParams = [name, email, passwordHash, roleID];
     try {
-        const newUser = await createUser(name, email, password);
-        res.status(201).json(newUser); // POST Created
+
+        const queryResult = await pool.query(queryText, queryParams);
+        res.status(201).json(queryResult); // POST Created
     } catch (error) {
         res.status(500).json({ error: "Database error" }); // Internal error
     }
