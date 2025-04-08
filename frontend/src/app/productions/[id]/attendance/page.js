9292765bb0cdcb8f2d51e3f696d5b144d4812e17
Lucas Jin <kblazer20@gmail.com>
2025-04-08 12:31:46 -0400

    Change date buttons and fix date for attendance

--- frontend/src/app/productions/[id]/attendance/page.js
+++ frontend/src/app/productions/[id]/attendance/page.js
@@ -1,5 +1,6 @@
 'use client';
 
+import styles from '../../../styles/attendance.module.css'
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation'; 
 
@@ -8,23 +9,24 @@ function ProdAttendancePage() {
 
     const [attendance, setAttendance] = useState([]);
     const [dates, setDates] = useState([]);
+    const [startDate, setStartDate] = useState(new Date());
 
     useEffect(() => {
         getProdAttendance();
-        getDates();
-    }, []);
+        getDates(startDate);
+    }, [startDate]);
 
-    const getDates = () => {
+    const getDates = (baseDate) => {
         var generatedDates = [];
-        var currentDate = new Date();
-        currentDate.setUTCHours(0,0,0,0);
+        var currentDate = new Date(baseDate);
+        currentDate.setUTCHours(0, 0, 0, 0);
         for (var i = 0; i < 7; i++) {
-            generatedDates.unshift(currentDate.toISOString());
-            currentDate.setDate(currentDate.getDate() - 1);
+            generatedDates.push(currentDate.toISOString().split('T')[0]);
+            currentDate.setUTCDate(currentDate.getUTCDate() + 1);
         }
         setDates(generatedDates);
     }
-
+    
     const getProdAttendance = async () => {
         const res = await fetch(`http://localhost:3001/productions/${id}/attendance`, {
             method: 'GET',
@@ -42,31 +44,47 @@ function ProdAttendancePage() {
         setAttendance(data.attendance);
     };
 
+    const nextWeek = () => {
+        const newStartDate = new Date(startDate);
+        newStartDate.setUTCDate(startDate.getUTCDate()+7);
+        setStartDate(newStartDate);
+    };
+
+    const lastWeek = () => {
+        const newStartDate = new Date(startDate);
+        newStartDate.setUTCDate(startDate.getUTCDate()-7);
+        setStartDate(newStartDate);
+    };
+
     return (
         <div>
             <h1>Attendance</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <td></td>
-                        {dates.map(date => (
-                            <th key={date} scope="col">
-                                {new Date(date).toLocaleDateString('en-CA', {weekday: 'short', month: 'short', day: 'numeric'})}
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {attendance.map(user => (
-                        <tr key={user.user_id}>
-                            <th scope="row">{user.name}</th>
+            <div className={styles.flex}>
+                <button onClick={lastWeek}>Previous Week</button>
+                <table className={styles.table}>
+                    <thead>
+                        <tr>
+                            <td className={styles.td}></td>
                             {dates.map(date => (
-                                <td key={date}>{user.attendedDates[date] === true ? (<span>Present</span>) : (<span>-</span>)}</td>
+                                <th key={date} scope="col" className={styles.th}>
+                                    {new Date(date).toLocaleDateString('en-CA', {weekday: 'short', month: 'short', day: 'numeric'})}
+                                </th>
                             ))}
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {attendance.map(user => (
+                            <tr key={user.user_id}>
+                                <th scope="row" className={styles.th}>{user.name}</th>
+                                {dates.map(date => (
+                                    <td key={date} className={styles.td}>{user.attendedDates[date] === true ? (<span>Present</span>) : (<span>-</span>)}</td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <button onClick={nextWeek}>Next Week</button>
+            </div>
         </div>
     );
 }
